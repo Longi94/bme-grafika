dd34bc5ed2291105d3f892a52cbd5a4bfd13641e
Long Tran <lngtrn94@gmail.com>
2015-10-30 22:01:00 +0100

    colors instead of vectors

--- grafika2/main.cpp
+++ grafika2/main.cpp
@@ -90,7 +90,7 @@ struct Vector {
 		return Vector(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
 	}
 	float Length() { return sqrt(x * x + y * y + z * z); }
-	Vector normalize() {
+	Vector norm() {
 		return Vector(x / Length(), y / Length(), z / Length());
 	}
 };
@@ -107,11 +107,23 @@ struct Color {
 	Color operator*(float a) {
 		return Color(r * a, g * a, b * a);
 	}
+	Color operator*(const Color& c) { 
+		return Color(r * c.r, g * c.g, b * c.b); 
+	}
+	Color operator+(const Color& c) { 
+		return Color(r + c.r, g + c.g, b + c.b); 
+	}
 	Color operator*(const Color& c) {
 		return Color(r * c.r, g * c.g, b * c.b);
 	}
 	Color operator+(const Color& c) {
 		return Color(r + c.r, g + c.g, b + c.b);
+	}
+	Color operator/(const Color& c) { 
+		return Color(r / c.r, g / c.g, b / c.b); 
+	}
+	Color operator-(const Color& c) {
+		return Color(r - c.r, g - c.g, b - c.b);
 	}
 };
 
@@ -129,21 +141,19 @@ struct Ray {
 class Material {
 protected:
 	//Smooth
-	Vector F0; //Fresnel cucc
-	float n; //toresmutato
+	Color F0; //Fresnel, anyagra jellemzo const: ((n-1)*(n-1) + k*k) / ((n+1)*(n+1) + k*k)
+	Color n; //toresmutato
 
 	//Rough
-	Vector kd, ks;
+	Color kd, ks; //diffuz tul (szin)
 	float shininess; //fenyesseg
 public:
 	virtual bool isReflective() = 0;
 	virtual bool isRefractive() = 0;
 	virtual Vector reflect(Vector& direction, Vector& normal) = 0;
 	virtual Vector refract(Vector& direction, Vector& normal) = 0;
-	virtual int getReflectance() = 0; //mennyire visszavero?
-	virtual int getTransmittance() = 0; //mennyire atengedo?
-	virtual Vector shade(Vector& normal, Vector& viewDir, Vector& lightDir, Vector& inRad) = 0;
-	virtual Vector Fresnel(Vector& direction, Vector& normal) = 0;
+	virtual Color shade(Vector& normal, Vector& viewDir, Vector& lightDir, Color& radIn) = 0;
+	virtual Color Fresnel(Vector& direction, Vector& normal) = 0;
 };
 
 //Talalat!!
@@ -160,6 +170,11 @@ struct Hit {
 //Sima felulet, a sugar siman visszaverodik es/vagy torik
 class SmoothMaterial : public Material {
 public:
+	SmoothMaterial(Color n, Color k) {
+		this->n = n;
+		this->F0 = (n - Color(1, 1, 1) * (n - Color(1, 1, 1)) + k*k) / ((n + Color(1, 1, 1))*(n + Color(1, 1, 1)) + k*k);
+	}
+
 	bool isReflective() {
 		return true;
 	}
@@ -173,7 +188,7 @@ public:
 	}
 
 	Vector refract(Vector& direction, Vector& normal) {
-		float ior = n;
+		Color ior = n;
 
 		float cosalpha = -(normal * direction);
 		if (cosalpha < 0) {
@@ -189,15 +204,25 @@ public:
 		return direction / ior + normal * (cosalpha / ior - sqrtf(disc));
 	}
 
-	Vector Fresnel(Vector& direction, Vector& normal) {
+	Color Fresnel(Vector& direction, Vector& normal) {
 		float cosalpha = fabsf(normal * direction);
-		return F0 + (Vector(1, 1, 1) - F0) * powf(1 - cosalpha, 5);
+		return F0 + (Color(1, 1, 1) - F0) * powf(1 - cosalpha, 5);
+	}
+
+	Color shade(Vector& normal, Vector& viewDir, Vector& lightDir, Color& radIn) {
+		return (Color(0, 0, 0)); //unused
 	}
 };
 
 //Rucskos anyag, a sugar szetverodik ezen, innen jon a szin
 class RoughMaterial : public Material {
 public:
+	RoughMaterial(Color kd, Color ks, float shininess) {
+		this->kd = kd;
+		this->ks = ks;
+		this->shininess = shininess;
+	}
+
 	bool isReflective() {
 		return false;
 	}
@@ -206,23 +231,35 @@ public:
 		return false;
 	}
 
-	Vector shade(Vector& normal, Vector& viewDir, Vector& lightDir, Vector& inRad) {
-		Vector reflRad = Vector(0, 0, 0);
+	Vector reflect(Vector& direction, Vector& normal) {
+		return Vector(0, 0, 0); //unused
+	}
 
-		float cosTheta = normal * lightDir;
+	Vector refract(Vector& direction, Vector& normal) {
+		return Vector(0, 0, 0); //unused
+	}
+
+	Color Fresnel(Vector& direction, Vector& normal) {
+		return Color(0, 0, 0); //unused
+	}
+
+	Color shade(Vector& normal, Vector& viewDir, Vector& lightDir, Color& radIn) {
+		Color radOut = Color(0, 0, 0);
+
+		float cosTheta = normal * lightDir; //Lambert trv
 		if (cosTheta < 0) {
-			return reflRad;
+			return radOut;
 		}
 
-		reflRad = inRad % kd * cosTheta;
-		Vector halfway = (normal + halfway).normalize();
+		radOut = radIn * kd * cosTheta;
+		Vector halfway = (normal + halfway).norm();
 
 		float cosDelta = normal * halfway;
 		if (cosDelta < 0) {
-			return reflRad;
+			return radOut;
 		}
 
-		return reflRad + inRad % ks * powf(cosDelta, shininess);
+		return radOut + radIn * ks * powf(cosDelta, shininess);
 	}
 };
 
@@ -238,6 +275,12 @@ public:
 class Plane : public Intersectable {
 	Vector position, normal;
 public:
+	Plane(Vector position, Vector normal, Material* material) {
+		this->position = position;
+		this->normal = normal;
+		this->material = material;
+	}
+
 	Hit intersect(const Ray& ray) {
 
 	}
@@ -286,16 +329,32 @@ struct Camera {
 	}
 };
 
-const float roomX = 10.0f;
-const float roomY = 10.0f;
-const float roomZ = 10.0f;
-
 Color image[Camera::XM*Camera::YM];
 
+Camera camera;
 Intersectable* objects[7];
 
-void build() {
+Plane wallLeft;
+Plane wallRight;
+Plane wallFront;
+Plane wallBack;
+Plane wallTop;
+Plane wallBottom;
 
+RoughMaterial rough;
+
+void build() {
+	//camera init
+	camera = Camera();
+	camera.eye = Vector(5, 5, 0);
+	camera.lookat = Vector(5, 5, 2);
+	camera.up = Vector(0, 1, 0);
+	camera.right = Vector(-1, 0, 0);
+
+	rough = RoughMaterial();
+
+	//flat walls init
+	wallLeft = Plane(Vector(10, 10, 10), Vector(-1, 0, 0));
 }
 
 Hit firstIntersect(Ray ray) {
